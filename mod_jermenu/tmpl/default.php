ea1a2e5baca6dc73e640e09a2f8c855db9bf8f98
Jeremy Robson <jeremy@dragonflyit.ca>
2018-06-01 13:32:31 -0400

    added mod jer menu

---
+++ mod_jermenu/tmpl/default.php
@@ -0,0 +1,112 @@
+<?php defined('_JEXEC') or die; ?>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false"
+                aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">
+                <img src="img/cscc_logo_notext.png" width="150px">
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle navbar-text" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        About Us
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#">Awards and Grants</a>
+                        </li>
+                        <li>
+                            <a href="#">CSCC Governance</a>
+                        </li>
+                        <li>
+                            <a href="#">CSCC Archives</a>
+                        </li>
+                        <li>
+                            <a href="#">About Logo</a>
+                        </li>
+                        <li>
+                            <a href="#">CSCC By-Laws</a>
+                        </li>
+                        <li>
+                            <a href="#">Interest Groups and Committees</a>
+                        </li>
+                        <li>
+                            <a href="#">What Is a Clinical Biochemist?</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle navbar-text" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        Membership
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="#">Test</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle navbar-text" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        Chemist Library
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle navbar-text" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        Academy
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle navbar-text" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        Events
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle navbar-text" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        Provincial Sections
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle navbar-text" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        <span class="icon">
+                            <i class="fa fa-search"></i>&nbsp;</span>
+                        <span class="text">Search</span>
+                    </a>
+                </li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#" class="navbar-text">
+                        <span class="icon">
+                            <i class="fa fa-home"></i>&nbsp;</span>
+                        <span class="text">Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="navbar-text">
+                        <span class="icon">
+                            <i class="fa fa-phone"></i>&nbsp;</span>
+                        <span class="text">Contact Us</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="navbar-text">
+                        <span class="icon">
+                            <i class="fa fa-sign-in"></i>&nbsp;</span>
+                        <span class="text">Login</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
