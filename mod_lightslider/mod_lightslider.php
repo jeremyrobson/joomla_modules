d5a6d060f40e528da7c9571f294176e615b0f001
Jeremy Robson <jeremy@dragonflyit.ca>
2018-06-12 21:22:45 +0000

    updates to lightslider

--- mod_lightslider/mod_lightslider.php
+++ mod_lightslider/mod_lightslider.php
@@ -5,12 +5,17 @@
 
     $config = JFactory::getConfig();
     $images_path = $params->get("images_path");
+    //todo: error handling if path is not valid
     $image_paths = scandir(JPATH_BASE . "/" . $images_path);
+
+    //todo: error handling if no files are in path
 
     //echo "<pre>"; print_r($image_paths); echo '</pre>';
 
     $files = preg_grep("/.*\.(jpg|jpeg|png|gif)$/", $image_paths);
     $files = preg_filter('/^/', JURI::base() . $images_path . "/", $files);
+
+    //todo: error handling if no valid images are found in folder
 
     //echo "<pre>"; print_r($files); echo '</pre>';
 
